a6af5b1f82713565f1b05a48577aa8ce1b770e3a
nekusu <nekusu.dev@gmail.com>
2021-11-11 13:25:58 -0300

    Refactor Project and Task classes

--- src/task.js
+++ src/task.js
@@ -1,28 +1,18 @@
 class Task {
-	#name;
-	#description;
-	#date;
-	#isChecked;
-
 	constructor(name, description, date) {
-		this.#name = name;
-		this.#description = description;
-		this.#date = date;
-		this.#isChecked = false;
+		this.name = name;
+		this.description = description;
+		this.date = date;
+		this.isChecked = false;
 	}
-
-	get name() { return this.#name; }
-	get description() { return this.#description; }
-	get date() { return this.#date; }
-	get isChecked() { return this.#isChecked; }
 
 	edit(name, description, date) {
-		this.#name = name;
-		this.#description = description;
-		this.#date = date;
+		this.name = name;
+		this.description = description;
+		this.date = date;
 	}
 	toggleCheck() {
-		this.#isChecked = !this.#isChecked;
+		this.isChecked = !this.isChecked;
 	}
 }
 
